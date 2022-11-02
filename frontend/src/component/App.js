9152b9f8055c07be7400381b4f28cd3f116fae65
Arsenii Machekhin <arsenium2@gmail.com>
2022-11-02 16:13:42 +0300

    пр15 1.0.6

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -47,6 +47,7 @@ function App() {
       auth.getContent(token)
         .then((res) => {
           if (res) {
+            console.log('При перезагрузке сработал токен чек');
             setEmail(res.email);
             setLoggedIn(true);
             history.push('/')
