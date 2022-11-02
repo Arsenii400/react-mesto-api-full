fc795044856478fb896da7e97f6019c2e592ed7b
Arsenii Machekhin <arsenium2@gmail.com>
2022-11-02 19:58:37 +0300

    пр15 1.2.2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -13,7 +13,6 @@ module.exports = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
